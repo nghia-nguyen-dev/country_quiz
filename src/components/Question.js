1abac13da8fb6fe377bb85fb0c3e2bacd25b4357
Nghia <hello.nghia22@gmail.com>
2021-03-02 14:21:35 -0500

    feat: Create Question component

---
+++ src/components/Question.js
@@ -0,0 +1,10 @@
+// pass in question and options to pick from App
+
+export default () => {
+	return (
+		<div className="question">
+			<h2></h2>
+			<div className="question__options"></div>
+		</div>
+	);
+};
