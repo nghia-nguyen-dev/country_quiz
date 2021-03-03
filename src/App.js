fbe937a2a7fcaef544dae79d18e4836e7e27888a
Nghia <hello.nghia22@gmail.com>
2021-03-02 19:11:20 -0500

    feat: Create function to get random country codes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Card from "./components/Card";
 import Question from "./components/Question";
+import countryCodes from "./data/countryCodes.json";
 
 const listOfQuestions = [
 	{
@@ -10,23 +11,48 @@ const listOfQuestions = [
 	},
 ];
 
-const getRandomNum = (ceiling) => {
-   return Math.floor(Math.random() * ceiling)
-}
+const generateQuestion = () => {
+	return;
+};
 
-const getRandomQuestion = (index) => {
-   return listOfQuestions[num]
-}
+const getRandomNum = (ceiling) => {
+	return Math.floor(Math.random() * ceiling);
+};
+
+const getRandomItem = (array) => {
+	return array[getRandomNum(array.length)];
+};
+
+// Build list of random country codes
+
+const listOfRandomCodes = (numberOftimes) => {
+	const temp = [];
+
+	for (let i = 1; i <= numberOftimes; i++) {
+		let randomCode;
+		do {
+			randomCode = getRandomItem(countryCodes);
+		} while (temp.includes(randomCode));
+		temp.push(randomCode);
+	}
+
+	return temp;
+};
+
+
+// Fetch data with those 10 codes
 
 export default () => {
-   const [ currentQuestion, setCurrentQuestion] = useState()
+	const [currentQuestion, setCurrentQuestion] = useState(
+		getRandomItem(listOfQuestions)
+	);
 
 	return (
 		<div className="app">
 			<div>
 				<h1>Country Quiz</h1>
 				<Card>
-					<Question question={listOfQuestions[0]} />
+					<Question question={currentQuestion} />
 				</Card>
 			</div>
 		</div>
